48c79cec7cba1c7b3cd51979dd274004cb4bb626
Gabriel Silva <gabriel.psilva226@gmail.com>
2021-02-24 10:01:49 -0300

    Aula 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 
+import Profile from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
-import Profile from "../components/Profile";
 
 import styles from "../styles/pages/Home.module.css";
 
@@ -20,7 +21,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
