657899322512fa5b6b5adbf1db3c07f14b34b16a
Gabriel Silva <gabriel.psilva226@gmail.com>
2021-02-25 08:51:33 -0300

    Aula 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 const Profile = () => {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/gabeps2.png" alt="Gabriel" />
@@ -8,7 +12,7 @@ const Profile = () => {
         <strong>Gabriel Silva</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
